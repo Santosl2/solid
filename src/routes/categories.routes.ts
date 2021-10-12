2ca294561e3d3ac07f6c8773624de72579d9a59a
Santosl2 <mfilype2019@gmail.com>
2021-10-12 17:17:32 -0300

    Upload de arquivos

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCase/createCategory';
 import { listCategoriesController } from '../modules/cars/useCase/listCategories';
+import multer from 'multer';
+import { importCategoryController } from '../modules/cars/useCase/importCategory';
+
 
 const categoriesRoutes = Router();
+const upload = multer({
+    dest: "./tmp"
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -11,5 +17,11 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
     return listCategoriesController.handle(request, response);
 });
+
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+
+    return importCategoryController.handle(request, response);
+})
 
 export { categoriesRoutes };
