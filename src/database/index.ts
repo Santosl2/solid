f1a0d0c09b4888e4328a52e79c4e9eeea1150de9
Santosl2 <mfilype2019@gmail.com>
2021-10-19 21:38:55 -0300

    Criaçao banco de dados

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import { createConnection, getConnectionOptions } from "typeorm";
+
+//createConnection();
+
+interface IOptions {
+    host: string;
+}
+
+getConnectionOptions().then(options => {
+    const newOptions = options as IOptions;
+
+    newOptions.host = 'database';
+    createConnection({
+        ...options,
+    });
+});
