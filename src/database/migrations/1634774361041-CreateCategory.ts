72e4ccec8fd734572164447d00c78bdf73961dd1
Santosl2 <mfilype2019@gmail.com>
2021-10-20 21:00:05 -0300

    Criaçao migrations

---
+++ src/database/migrations/1634774361041-CreateCategory.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateCategory1634774361041 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
