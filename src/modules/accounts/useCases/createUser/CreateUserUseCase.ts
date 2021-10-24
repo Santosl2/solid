fa9e90bccebab7947411f19c4025e21a49b4f256
Santosl2 <mfilype2019@gmail.com>
2021-10-24 19:19:29 -0300

    Refatoraçao de codigo

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { hash } from "bcrypt";
 
 @injectable()
 class CreateUserUseCase {
@@ -11,9 +12,18 @@ class CreateUserUseCase {
     ) { }
 
     async execute({ name, password, email, driver_license }: ICreateUserDTO) {
+
+        const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+        if (userAlreadyExists) {
+            throw new Error("User already exists");
+        }
+
+        const passwordHash = await hash(password, 8);
+
         await this.usersRepository.create({
             name,
-            password,
+            password: passwordHash,
             email,
             driver_license
         })
