01cc4d8461a968656c04154853a942b06c119961
Santosl2 <mfilype2019@gmail.com>
2021-10-24 22:20:38 -0300

    Refatoraçao upload #1

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import multer from "multer";
+import uploadConfig from "../config/Upload";
 
 const userRoutes = Router();
-
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
+
 userRoutes.post("/", createUserController.handle);
+
+userRoutes.patch("/", uploadAvatar.single("file"), updateUserAvatarController.handle);
 
 export { userRoutes };
