30e6724d99343b01a4a03d295d11f633e8a9146c
Santosl2 <mfilype2019@gmail.com>
2021-10-26 20:46:44 -0300

    Criaçao de testes de Categoria e Autenticaçao

---
+++ src/modules/accounts/useCases/authUser/AuthenticateUserUseCase.spec.ts
@@ -0,0 +1,64 @@
+import { AppError } from "../../../../errors/AppError";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
+import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
+import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
+
+
+let authUserUseCase: AuthenticateUserUseCase;
+let createUsersUseCase: CreateUserUseCase;
+let usersRepositoryInMemory: UsersRepositoryInMemory;
+
+describe("Authenticate User", () => {
+    beforeEach(() => {
+        usersRepositoryInMemory = new UsersRepositoryInMemory();
+        authUserUseCase = new AuthenticateUserUseCase(usersRepositoryInMemory);
+        createUsersUseCase = new CreateUserUseCase(usersRepositoryInMemory);
+
+    });
+
+    it("should be able to auth an user", async () => {
+        const user: ICreateUserDTO = {
+            driver_license: "000",
+            email: "user@jest.com",
+            password: "jest",
+            name: "Jest"
+        }
+
+        await createUsersUseCase.execute(user);
+
+        const result = await authUserUseCase.execute({
+            email: user.email,
+            password: user.password
+        });
+
+        expect(result).toHaveProperty("token");
+    });
+
+    it("should not be able to auth an non existent user", () => {
+        expect(async () => {
+            await authUserUseCase.execute({
+                email: "false@markzuckerbeg.com",
+                password: "122"
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+
+    it("should not be able to auth with incorret password", () => {
+        expect(async () => {
+            const user: ICreateUserDTO = {
+                driver_license: "777",
+                email: "jesus@lovesme.god",
+                password: "jesusaves",
+                name: "Im Loved by jesus"
+            };
+
+            await createUsersUseCase.execute(user);
+
+            await authUserUseCase.execute({
+                email: user.email,
+                password: "jesusnotsave" // Oops, errou ein
+            });
+        }).rejects.toBeInstanceOf(AppError);
+    });
+});
