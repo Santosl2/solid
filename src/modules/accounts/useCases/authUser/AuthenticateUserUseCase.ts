1f7ab42864f198a515f99d6cf56160c6a9510b3d
Santosl2 <mfilype2019@gmail.com>
2021-10-26 21:13:08 -0300

    Refatoraçao importaçoes

--- src/modules/accounts/useCases/authUser/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authUser/AuthenticateUserUseCase.ts
@@ -1,8 +1,8 @@
 import { sign } from "jsonwebtoken";
 import { compare } from "bcrypt";
 import { inject, injectable } from "tsyringe";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
-import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { AppError } from "@errors/AppError";
 
 interface IRequest {
     email: string;
