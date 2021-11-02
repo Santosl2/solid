0214ce4599fcf1b7469955b96e637c7f8739a589
Santosl2 <mfilype2019@gmail.com>
2021-11-02 16:06:58 -0300

    Criaçao testesde aluguel

--- src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -1,2 +1,26 @@
+import { RentalsRepositoryInMemory } from "../repositories/in-memory/RentalsRepositoryInMemory";
+import { CreateRentalUseCase } from "./CreateRentalUseCase"
 
-describe("Create Rental")
+let rentalInMemory: RentalsRepositoryInMemory;
+let createRentalUseCase: CreateRentalUseCase;
+
+describe("Create Rental", () => {
+
+    beforeEach(() => {
+        rentalInMemory = new RentalsRepositoryInMemory();
+        createRentalUseCase = new CreateRentalUseCase(rentalInMemory);
+    });
+
+    it("should be able to create a new rental", async () => {
+        const rental = await createRentalUseCase.execute({
+            user_id: "123",
+            car_id: "00077",
+            expected_return_date: new Date()
+        });
+
+
+        expect(rental).toHaveProperty("id");
+        expect(rental).toHaveProperty("start_date");
+    });
+
+})
--- src/modules/rentals/useCases/CreateRentalUseCase.ts
+++ src/modules/rentals/useCases/CreateRentalUseCase.ts
@@ -1,8 +1,44 @@
+import { AppError } from "@shared/errors/AppError";
+import { Rental } from "../infra/typeorm/entities/Rental";
+import { IRentalsRepository } from "../repositories/IRentalsRepository";
+
+interface IRequest {
+    user_id: string;
+    car_id: string;
+    expected_return_date: Date;
+}
 
 class CreateRentalUseCase {
+    constructor(
+        private rentalsRepository: IRentalsRepository
+    ) { }
 
-    async execute(): Promise<void> {
+    async execute({
+        user_id,
+        car_id,
+        expected_return_date
+    }: IRequest): Promise<Rental> {
 
+        const carAlreadyRented = await this.rentalsRepository.findByCar(car_id);
+
+        if (carAlreadyRented) {
+            throw new AppError("Esse carro já está alugado.");
+        }
+
+        const userAlreadyRentedCar = await this.rentalsRepository.
+            findOpenRentalByUser(user_id);
+
+        if (userAlreadyRentedCar) {
+            throw new AppError("Este usuário já tem um carro alugado.");
+        }
+
+        const rental = await this.rentalsRepository.create({
+            user_id,
+            car_id,
+            expected_return_date
+        });
+
+        return rental;
     }
 
 }
